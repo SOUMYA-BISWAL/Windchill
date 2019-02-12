718c9ab00681d0fcc41d1124f12bf99ce7f177ae
sOuMyA_pRoGrAmMiNg_wOrLd <39147794+SOUMYA-BISWAL@users.noreply.github.com>
2019-02-12 19:50:32 +0530

    Create Part

---
+++ CreatePart.java
@@ -0,0 +1,28 @@
+package ext.document;
+import wt.doc.WTDocument;
+import wt.fc.Persistable;
+import wt.fc.PersistenceHelper;
+import wt.inf.container.WTContainerHelper;
+import wt.inf.container.WTContainerRef;
+import wt.part.WTPart;
+import wt.util.WTException;
+import wt.util.WTPropertyVetoException;
+public class CreatePart {
+
+	public static void main(String[] args) throws WTPropertyVetoException, WTException {
+		String container_path = "/wt.inf.container.OrgContainer=Demo Organization/wt.pdmlink.PDMLinkProduct=GOLF_CART";
+		String partName="Part1";
+		String partNumber="Part123";
+		System.out.println("Variable Declare ");
+		WTPart part = WTPart.newWTPart();
+		part.setName(partName);
+		part.setNumber(partNumber);
+		System.out.println("name and number input successfully ");
+		WTContainerRef containerRef = WTContainerHelper.service.getByPath(container_path);
+		part.setContainerReference(containerRef);
+		part = (WTPart) PersistenceHelper.manager.store((Persistable)part);
+		System.out.println("Successfull created "+ part);
+
+	}
+
+}
